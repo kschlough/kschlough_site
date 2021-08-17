6a59bcfcead4b4c9de40a61bb11a0d0ec78b6226
kschlough <61514488+kschlough@users.noreply.github.com>
2021-08-17 12:18:00 -0700

    adding bio box

---
+++ static/homepage.jsx
@@ -0,0 +1,25 @@
+'use strict';
+// bio box
+// const e = React.createElement;
+
+// class BioBox extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {read: false};
+//     }
+
+//     render() {
+//         if (this.state.liked) {
+//             return 'You liked this';
+//         } 
+
+//         return e(
+//             'button',
+//             { onMouseOver: () => this.setState({ read: true}) },
+//             'Like'
+//         );
+//     }
+// }
+
+// const domContainer = document.querySelector('#bio');
+// ReactDOM.render(e(BioBox), domContainer);
