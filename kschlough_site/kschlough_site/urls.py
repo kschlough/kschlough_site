cd95c29fc7f45a934c75c24194acf3099066be6d
kschlough <61514488+kschlough@users.noreply.github.com>
2021-08-20 11:09:40 -0700

    added home_view import to urls.py

--- kschlough_site/kschlough_site/urls.py
+++ kschlough_site/kschlough_site/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
