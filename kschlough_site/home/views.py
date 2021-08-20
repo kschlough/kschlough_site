300313f8f77c60cd78c04ff74617adf423b27a81
kschlough <61514488+kschlough@users.noreply.github.com>
2021-08-20 11:49:27 -0700

    added test blogpost through admin page

--- kschlough_site/home/views.py
+++ kschlough_site/home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import BlogPost
 
 def home_view(request):
+    # home view defines QuerySet (group of objects, qs) & pipes that into list of objects as the context
     qs = BlogPost.objects.all()
     template_name = 'home.html'
     context = {'object_list': qs}
